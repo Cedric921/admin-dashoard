20f16160ae42660467792b8d390ca3a6b248aadd
cedric karungu <ckarungu921@gmail.com>
2023-01-29 22:57:47 +0200

    feat:add employees table with filters

--- src/pages/Employees.tsx
+++ src/pages/Employees.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import { Header } from '../components';
+import {
+	GridComponent,
+	ColumnDirective,
+	ColumnsDirective,
+	Inject,
+	Resize,
+	Sort,
+	ContextMenu,
+	Filter,
+	Page,
+	Edit,
+	Search,
+	Toolbar,
+} from '@syncfusion/ej2-react-grids';
 
-const Employees = () => {
-	return <div>Employees</div>;
+import { employeesData, employeesGrid } from '../data/dummy';
+
+const Employee = () => {
+	return (
+		<div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+			<Header title='Employee' category='page' />
+			<GridComponent
+				dataSource={employeesData}
+				allowReordering
+				allowPaging
+				allowSorting
+				toolbar={['Search']}
+				width='auto'
+			>
+				<ColumnsDirective>
+					{employeesGrid.map((item, i) => (
+						<ColumnDirective key={i} {...item} />
+					))}
+				</ColumnsDirective>
+				<Inject
+					services={[Resize, Sort, ContextMenu, Search, Page, Toolbar, Edit]}
+				/>
+			</GridComponent>
+		</div>
+	);
 };
 
-export default Employees;
+export default Employee;
