f38b01018534e816834b5887c322a98c4e5db3a6
cedric karungu <ckarungu921@gmail.com>
2023-01-31 16:38:38 +0200

    feat: add color picker

--- src/pages/ColorPicker.tsx
+++ src/pages/ColorPicker.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Header } from '../components';
 
 const ColorcPiker = () => {
-	return <div>ColorcPiker</div>;
+	return (
+		<div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+			<Header title='Calendar' category='App' />
+		</div>
+	);
 };
 
 export default ColorcPiker;
